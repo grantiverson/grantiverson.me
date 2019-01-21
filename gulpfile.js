067d13d7003be4b3f7bcc3e37cb0500aa4007f66
Grant Iverson <iversonga@gmail.com>
2019-01-20 22:03:14 -0500

    Updated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
 let gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    coffee = require('gulp-coffee'),
-    connect = require('gulp-connect'),
-    uglify = require('gulp-uglify'),
-    eslint = require('gulp-eslint'),
-    concat = require('gulp-concat'),
-    babel = require('gulp-babel'),
-    sourcemaps = require('gulp-sourcemaps'),
-    imagemin = require('gulp-imagemin'),
-    imageminPngquant = require('imagemin-pngquant');
+  gutil = require('gulp-util'),
+  sass = require('gulp-sass'),
+  coffee = require('gulp-coffee'),
+  connect = require('gulp-connect'),
+  uglify = require('gulp-uglify'),
+  eslint = require('gulp-eslint'),
+  concat = require('gulp-concat'),
+  babel = require('gulp-babel'),
+  sourcemaps = require('gulp-sourcemaps'),
+  imagemin = require('gulp-imagemin'),
+  imageminPngquant = require('imagemin-pngquant');
 
 let coffeeSources = ['build/js/hello.coffee'],
-    scriptsSources = ['build/js/*.js'],
-    stylesSources = ['build/css/*.sass'],
-    htmlSources = ['build/*.html'],
-    imagesSources = ['build/img/*'],
-    outputDir = 'dist';
+  scriptsSources = ['build/js/*.js'],
+  stylesSources = ['build/css/*.sass'],
+  htmlSources = ['build/*.html'],
+  imagesSources = ['build/img/*'],
+  outputDir = 'dist';
 
-gulp.task('copy-html', function() {
+const copyHtml = () => (
   gulp.src(htmlSources)
     .pipe(gulp.dest(outputDir))
     .pipe(connect.reload())
-});
+);
 
 // gulp.task('--default', function() {
 //   gulp.src(imagesSources)
@@ -39,53 +39,53 @@ gulp.task('copy-html', function() {
 //     .pipe(gulp.dest(outputDir + '/img'));
 // });
 
-gulp.task('copy-images', function() {
+const copyImages = () => (
   gulp.src(imagesSources)
     .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
       imagemin.svgo({
         plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
+          { removeViewBox: true },
+          { cleanupIDs: false }
         ]
       })
     ]))
     .pipe(gulp.dest(outputDir + '/img'))
-});
+);
 
-gulp.task('copy-work-dir', function() {
+const copyWorkDir = () => (
   gulp.src('build/work/**/*')
     .pipe(gulp.dest(outputDir + '/work'))
-});
+);
 
-gulp.task('styles', function() {
+const styles = () => (
   gulp.src(stylesSources)
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
-      .on('error', gutil.log)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .on('error', gutil.log)
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(outputDir + '/css'))
     .pipe(connect.reload())
-});
+);
 
-gulp.task('coffee', function() {
-  gulp.src(coffeeSources)
-    .pipe(coffee({bare: true})
-      .on('error', gutil.log))
-    .pipe(gulp.dest(outputDir + '/js'))
-});
+// const runCoffee = () => (
+//   gulp.src(coffeeSources)
+//     .pipe(coffee({ bare: true })
+//       .on('error', gutil.log))
+//     .pipe(gulp.dest(outputDir + '/js'))
+// );
 
-gulp.task('scripts', function() {
+const scripts = () => (
   gulp.src(scriptsSources)
     .pipe(babel())
     .pipe(concat('app.js'))
     .pipe(gulp.dest(outputDir + '/js'))
     .pipe(connect.reload())
-});
+);
 
-gulp.task('scripts-dist', function() {
+const scriptsDist = () => (
   gulp.src(scriptsSources)
     .pipe(babel())
     .pipe(sourcemaps.init())
@@ -94,29 +94,29 @@ gulp.task('scripts-dist', function() {
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(outputDir + '/js'))
     .pipe(connect.reload())
-});
+);
 
-gulp.task('watch', function() {
-  gulp.watch(htmlSources, ['copy-html']);
-  gulp.watch(stylesSources, ['styles']);
-  gulp.watch(coffeeSources, ['coffee']);
-  gulp.watch(scriptsSources, ['scripts']);
-  // gulp.watch(scriptsSources, ['lint']);
-});
+const watch = () => {
+  gulp.watch(htmlSources, copyHtml);
+  gulp.watch(stylesSources, styles);
+  gulp.watch(coffeeSources, runCoffee);
+  gulp.watch(scriptsSources, scripts);
+  // gulp.watch(scriptsSources, lint);
+};
 
-gulp.task('connect', function() {
+const connectBrowser = () => {
   connect.server({
     root: '.',
     livereload: true
   });
-});
+};
 
-gulp.task('lint', function() {
+const lint = () => {
   // ESLint ignores files with "node_modules" paths.
   // So, it's best to have gulp ignore the directory as well.
   // Also, Be sure to return the stream from the task;
   // Otherwise, the task may end before the stream has finished.
-  return gulp.src(['**/*.js','!node_modules/**','!build/js/app.js'])
+  gulp.src(['**/*.js', '!node_modules/**', '!build/js/app.js'])
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
     .pipe(eslint())
@@ -126,25 +126,28 @@ gulp.task('lint', function() {
     // To have the process exit with an error code (1) on
     // lint error, return the stream and pipe to failAfterError last.
     .pipe(eslint.failAfterError());
-});
+};
 
-gulp.task('dist', [
-  'copy-html',
-  'copy-images',
-  'styles',
-  // 'lint',
-  'scripts-dist',
-  'copy-work-dir',
-  // 'compress-images'
-])
+const dist = gulp.series(
+  copyHtml,
+  copyImages,
+  styles,
+  // lint,
+  scriptsDist,
+  copyWorkDir,
+  // compressImages
+);
 
-gulp.task('default', [
-  'copy-html',
-  'copy-images',
-  'styles',
-  'scripts',
-  'coffee',
-  'connect',
-  'watch',
-  'copy-work-dir'
-]);
+const run = gulp.series(
+  copyHtml,
+  copyImages,
+  styles,
+  scripts,
+  // runCoffee,
+  connectBrowser,
+  watch,
+  copyWorkDir
+);
+
+exports.dist = dist;
+exports.default = run;
